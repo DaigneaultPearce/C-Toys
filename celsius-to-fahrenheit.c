e0fa56744d56a35fc3f4870c8d665d8a21083e70
Daigneault Pearce <Daigneault.PardoPearce.515@my.csun.edu>
2018-04-17 13:17:14 -0700

    reverse table order

--- celsius-to-fahrenheit.c
+++ celsius-to-fahrenheit.c
@@ -5,7 +5,7 @@ int main(){
 	float celsius;
 	
 	printf("C\tF\n");
-	for ( celsius = 0; celsius <= 1000; celsius = celsius + 20)
+	for ( celsius = 1000; celsius >= 0; celsius = celsius - 20)
 		printf( "%4.0f \t %4.0f\n", celsius, (9.0/5.0) * celsius + 32 );
 	
 	return 0;
