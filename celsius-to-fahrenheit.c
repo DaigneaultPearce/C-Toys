559027b7e6e0c83309fceb98b878de651d9fafe1
DaigneaultPearce <daigneault.pardopearce.515@my.csun.edu>
2018-04-17 20:30:31 -0700

    Replace variables with named constants

--- celsius-to-fahrenheit.c
+++ celsius-to-fahrenheit.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#define UPPER 1000 //Upper limit of table
+#define LOWER 0	   //Lower limit of table
+#define STEP 20	   //Step size between entries
 
 int main(){
 	
 	float celsius;
 	
 	printf("C\tF\n");
-	for ( celsius = 1000; celsius >= 0; celsius = celsius - 20)
+	for ( celsius = UPPER; celsius >= LOWER; celsius = celsius - STEP)
 		printf( "%4.0f \t %4.0f\n", celsius, (9.0/5.0) * celsius + 32 );
 	
 	return 0;
