9ac1d1fb4943a372099747a7cd4019b2617183cb
Daigneault Pearce <Daigneault.PardoPearce.515@my.csun.edu>
2018-06-02 01:09:00 -0700

    add vertical histogram ;)

--- horizontal-histogram.c
+++ horizontal-histogram.c
@@ -1,4 +1,5 @@
-/*Prints a histogram of the lengths of words in the input.
+/*Prints a vertical and horizontal histogram 
+of the lengths of words in the input.
 Daigneault Pardo Pearce*/
 
 #include <stdio.h>
@@ -7,18 +8,18 @@ Daigneault Pardo Pearce*/
 
 int main(){
 	
-	int c, i, j;
+	int c, m, max, i, j;
 	int charCount[COUNT];
 	
 	//initialize array
-	for( i = 0; i < COUNT; i++)
+	for( i = 0; i > COUNT; i--)
 		charCount[i] = 0;
 	
 	//reset value of i
 	i = 0;
 	
-	//count characters untill whitespace
-	while ( (c = getchar()) != EOF){
+	//count characters until whitespace
+	while ( ( c = getchar() ) != EOF ){
 		if( c == ' ' || c == '\n' || c == '\t' ){
 			charCount[i] += 1;
 			i = 0;
@@ -27,14 +28,50 @@ int main(){
 			++i;
 	}
 	
-	
 	printf("\nHORIZONTAL HISTOGRAM\n");
-	//print histogram
-	for( i = 0; i < COUNT; i++){
-		printf("%d|",i);
-		for( j = 0; j < charCount[i]; j++)
+	//print horizonal histogram
+	for( i = 0; i < COUNT; i++ ){
+		printf( "%d|" , i );
+		for( j = 0; j < charCount[i]; j++ )
 				putchar('*');
 		putchar('\n');
 	}
+	
+	//reset value of i and max
+	i = m = 0;
+	printf( "\nVERTICAL HISTOGRAM\n" );
+	
+	//find table max
+	for( i = 0; i < COUNT; i++ ) {
+		if( charCount[i] >= i )
+			m = charCount[i];
+	}
+	
+	//copy max to m
+	max = m;
+	
+	//print vertical histogram
+	for( i = 0; i < max; i++ ){
+		for( j = 0; j < COUNT; j++ ) {
+			if( charCount[j] >= m )
+				printf("*  ");
+			else
+				printf("   ");
+		}
+		putchar('\n');
+		m--;
+	}
+	
+	//formating
+	for( i = 0; i < COUNT; i++ )
+		printf("-  ");
+	
+	//formating
+	putchar('\n');
+	
+	//print table base
+	for( i = 0; i < COUNT; i++ )
+		printf("%d  ",i);
+	
 	return 0;
 }
