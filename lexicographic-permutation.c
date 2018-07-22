b3c29e333cbc919bd9e7aa24d011ea546f512298
DaigneaultPearce <daigneault.pardopearce.515@my.csun.edu>
2018-07-21 22:28:16 -0700

    implement Narayana Pandits algo for generating lexicographioc permutations

---
+++ lexicographic-permutation.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int next_permutation(int n, char **s)
+{
+    int hasNext, k, l, i;
+    char *tmp;
+    hasNext = 0;
+    //find k
+    for(i = n - 1; i > 0; i--){
+        if( s[i - 1] < s[i]){
+            k = i - 1;
+            printf("k = %d\n", k);
+            hasNext = 1;
+            break;
+        }
+    }
+    //find l
+    for(i = n - 1; i > k; i--){
+        if( s[i] > s[k]){
+            printf("l = %d\n", l);
+            l = i;
+            break;
+        }
+    }
+    //swap k and l
+    printf("swapping %s and %s\n", s[k], s[l]);
+    tmp = s[k];
+    s[k] = s[l];
+    s[l] = tmp;
+    //reverse from k+1 to n
+    for( i = k + 1; i < n - 1; i++ ){
+        printf("reverse %s and %s\n", s[i], s[i + 1]);
+        tmp = s[i];
+        s[i] = s[i + 1];
+        s[i + 1] = tmp;
+    }
+	/**
+	* Complete this method
+	* Return 0 when there is no next permutation and 1 otherwise
+	* Modify array s to its next permutation
+	*/
+    return hasNext;
+}
+int main()
+{
+	char **s;
+	int n;
+	scanf("%d", &n);
+	s = calloc(n, sizeof(char*));
+	for (int i = 0; i < n; i++)
+	{
+		s[i] = calloc(11, sizeof(char));
+		scanf("%s", s[i]);
+	}
+	do
+	{
+		for (int i = 0; i < n; i++)
+			printf("%s%c", s[i], i == n - 1 ? '\n' : ' ');
+	} while (next_permutation(n, s));
+	for (int i = 0; i < n; i++)
+		free(s[i]);
+	free(s);
+	return 0;
+}
